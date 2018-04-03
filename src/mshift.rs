5c1b6fd80dd7fb8685c18e55ca951f98983d4103
Dmitry Khominich <khdmitryi@gmail.com>
2018-04-03 08:40:09 +0300

    [*] mshift: test

--- src/mshift.rs
+++ src/mshift.rs
@@ -253,18 +253,43 @@ mod test {
         }
     }
 
-    // #[test]
-    // fn foursquare() {
-    //     let filename = "/Users/dmitry/code/courses/coursera/machine_learning_yandex/unsupervised_learning_w1/checkins_proc.dat";
-    //     let train: DMatrix<f64> = DMatrix::from_csv(filename, 1, ',', Some(&[0, 1])).unwrap();
-    //     let mut ms = MeanShift::new( MeanShiftOptions::new()
-    //                                        .bandwidth(0.3)
-    //                                        .seed(SeedOptions::RandomChoice(1000))
-    //                                );
-    //     ms.fit(&train).unwrap();
-    //     println!("n_clusters = \n{:?}", ms.n_clusters());
-    //     println!("centers = \n{}", ms.centers);
-    // }
+    #[test]
+    #[ignore]
+    fn foursquare() {
+        let filename = "data/checkins_proc_100k.dat";
+        let train: DMatrix<f64> = DMatrix::from_csv(filename, 1, ',', Some(&[0, 1])).unwrap();
+        let offices: DMatrix<f64> = DMatrix::from_csv("data/offices.csv", 1, ',', Some(&[0, 1])).unwrap();
+        let mut ms = MeanShift::new( MeanShiftOptions::new()
+                                           .bandwidth(0.1)
+                                           .seed(SeedOptions::Bins(0.1, 1))
+                                   );
+        ms.fit(&train).unwrap();
+        println!("n_clusters = \n{:?}", ms.n_clusters());
+        println!("centers = \n{}", ms.centers);
+
+        // Ищем для каждого кластера ближайший офис
+        let mut kdtree = KdTree::new_with_capacity(offices.cols(), offices.rows());
+        (0..offices.rows()).for_each(|i| kdtree.add(offices.get_row(i), i).unwrap());
+        let mut min_dist = Vec::with_capacity(ms.n_clusters());
+        for i in 0..ms.n_clusters() {
+            kdtree.nearest(ms.centers.get_row(i), 1, &squared_euclidean).unwrap()
+                .iter().take(1).for_each(|&(d, i)| min_dist.push(d.sqrt()));
+        }
+        // сортируем по возрастанию расстояния до офиса
+        let mut ids = (0..ms.n_clusters()).collect::<Vec<usize>>();
+        ids.sort_unstable_by(|&a, &b| min_dist[a].partial_cmp(&min_dist[b]).unwrap());
+
+        // Печатаем 20 ближайших кластеров
+        println!("Nearest clusters:");
+        for i in 0..20 {
+            println!("Cluster {}: dist {}, coord {:?}", i + 1, min_dist[ids[i]], ms.centers.get_row(ids[i]));
+        }
+
+        // Проверяем координаты ближайшего
+        let closest = ms.centers.get_row(ids[0]);
+        assert!((closest[0] - (-33.861)).abs() < 1e-3);
+        assert!((closest[1] - (151.205)).abs() < 1e-3);
+    }
 
     #[test]
     fn get_bin_seeds_case1() {
